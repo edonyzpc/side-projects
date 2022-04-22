834d20d746735bd6553ba19e4a51720f41c2a8c7
edonyzpc <edonyzpc@yahoo.com>
2022-04-22 08:23:05 +0000

    feat: add shadow walker sitemap parser
    1. init shadow walker sitemap parser tool
    2. add github action to get sitemap parser running
    
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # side-projects
 Personal side project about my product idea and prototype.
+
+[![Shadow Walker Actions](https://github.com/edonyzpc/side-projects/actions/workflows/shadowwalker-xml-parse.yml/badge.svg)](https://github.com/edonyzpc/side-projects/actions/workflows/shadowwalker-xml-parse.yml)
---
+++ sitemap-parser/sitemap-parse.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib.request
+from xml.dom.minidom import parseString
+
+if __name__ == "__main__":
+    posts_xml = urllib.request.urlopen(
+        'https://www.edony.ink/sitemap-posts.xml').read()
+
+    dom_tree = parseString(posts_xml)
+    posts = []
+    for item in dom_tree.getElementsByTagName("url"):
+        post = item.childNodes[0].childNodes[0].data
+        print(post)
+        posts.append(post)
+
+    print(f"total post: {len(posts)}")
