bbe0eb8d0866dcc96016d41e5d75307b50c57b7b
pengchen.zpc <pengchen.zpc@antfin.com>
2022-08-03 18:35:48 +0800

    fix: fix ci error about 403 forbidden
    Signed-off-by: pengchen.zpc <pengchen.zpc@antfin.com>

--- sitemap-parser/sitemap-parse.py
+++ sitemap-parser/sitemap-parse.py
@@ -4,10 +4,23 @@
 import urllib
 import urllib.request
 from xml.dom.minidom import parseString
+from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    posts_xml = urllib.request.urlopen(
-        'https://www.edony.ink/sitemap-posts.xml').read()
+    head = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive',
+            'refere': 'https://edony.ink',
+            }
+    req = Request('https://www.edony.ink/sitemap-posts.xml', headers=head)
+    posts_xml = urlopen(req).read()
+
+    #posts_xml = urllib.request.urlopen(
+    #    'https://www.edony.ink/sitemap-posts.xml').read()
 
     dom_tree = parseString(posts_xml)
     posts = []
